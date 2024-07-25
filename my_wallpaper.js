ed907d94904674ac6199d4d6c68aafdcf3e353e7
JehSikah <jess.tia.merriman@gmail.com>
2024-07-25 16:58:20 +1200

    finished ducky

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,43 +1,54 @@
 //your parameter variables go here!
-let smol = 25;
+
+let mover = 0; //rotate (degrees)
+let movex = 0; //move on x
+let movey = 0; //move on y
+let moves = 2.5; //scale - default 1
 
 //colour variables
-let greem;
-let greemer;
-let reb;
-let wite;
-let bleck;
-let yell;
+let green;
+let dark_green;
+let red;
+let white;
+let black;
 let yellow;
+let dark_yellow;
+let orange;
+let dark_orange;
+
+let positions = [];
+let rotations = [];
+let scales = [];
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   //DEVELOP_GLYPH, GRID_WALLPAPER, GLIDE_WALLPAPER
   pWallpaper.resolution(FIT_TO_SCREEN);
   //FIT_TO_SCREEN, NINE_LANDSCAPE, NINE_PORTRAIT, A4, A3, 
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
   pWallpaper.grid_settings.row_offset  = 100;
 
-  angleMode(DEGREES);
-
   //Defining colours
-  greem = color(20, 250, 20);
-  greemer = color(20, 200, 20);
-  reb = color(255, 0, 21);
-  wite = color(255, 255, 255);
-  bleck = color(0,0,0);
-  yell = color(235, 235, 150);
-  yellow = color(235, 235, 110);
+  green = color(20, 250, 20);
+  dark_green = color(20, 200, 20);
+  red = color(255, 0, 21);
+  white = color(255, 255, 255);
+  black = color(0,0,0);
+  yellow = color(255, 235, 120);
+  dark_yellow = color(230, 210, 100);
+  orange = color(255, 180, 100);
+  dark_orange = color(235, 160, 85);
+
 }
 
 function wallpaper_background() {
   //background(240, 255, 240); //light honeydew green colour
   //background(150, 150, 225); //bluey purple thing
-  background(170, 170, 225);
+  background(46, 227, 255);
 }
 
 function fillIn(colour) {
@@ -46,18 +57,20 @@ function fillIn(colour) {
 }
 
 function kermi() {
-  fillIn(greem);  
+  let smol = 25;
+
+  fillIn(green);  
   rect(70, 170, 20, 30); //leg l
   rect(110, 170, 20, 30); //leg r
   rect(0, 120, 200, 20); //arm
 
-  fillIn(greemer);
+  fillIn(dark_green);
   ellipse(100, 153, 80, 90);//body shadow
 
-  fillIn(greem);
+  fillIn(green);
   ellipse(100, 150, 80, 90); //body
 
-  fillIn(greemer);
+  fillIn(dark_green);
   ellipse(100, 113, 60, 40) //neck shadow
 
   triangle(70, 110, 90, 130, 80, 145); //tri l
@@ -67,23 +80,22 @@ function kermi() {
   fillIn(greem);
   ellipse(100, 95, 70, 70); //head
 
-  fillIn(greem);
   ellipse(70, 95, smol, smol); //cheek l
   ellipse(130, 95, smol, smol); //cheek r
   ellipse(85, 70, smol, smol); //eye l
   ellipse(115, 70, smol, smol); //eye r
 
-  fillIn(reb);
+  fillIn(red);
   arc(100, 100, 50, 40, PI, 0); //smile
 
-  fillIn(greem);
+  fillIn(green);
   triangle(70, 100, 130, 100, 100, 105); //up lip
 
-  fillIn(wite);
+  fillIn(white);
   arc(85, 75, 20, 25, PI, 0); //eyeball l
   arc(115, 75, 20, 25, PI, 0); //eyeball r
 
-  fillIn(bleck);
+  fillIn(black);
   ellipse(85, 72, 6, 6); //pupil l
   ellipse(115, 72, 6, 6); //pupil r
   line(77, 73, 94, 71); //eyestick l
@@ -91,24 +103,58 @@ function kermi() {
 }
 
 function ducky() {
-  var dhead = 40;
-  var dbod = 60;
+  let dhead = 40;
+  let dbod = 60;
+  let eyew = 8;
+  let eyeh = 3;
+
+  rotate(45); //wings on angle - 45 degree
+  fillIn(dark_yellow);  
+  ellipse(55.5, 0, dbod/3, 50); //left wing shadow
+  ellipse(100, 0, dbod/3, 50); //right wing shadow
 
   fillIn(yellow);
-  triangle(30, 50, 60, 80, 30, 80);
-  ellipse(60, 50, dbod, dbod);
+  ellipse(55.5, 0, dbod/3.5, 45); //left wing light
+  ellipse(100, 0, dbod/3.5, 45); //right wing light
 
-  rotate(45);
-  ellipse(55, 0, dbod/3, 50);
-  ellipse(100, 0, dbod/3, 50);
+  rotate(-45); //reset angle - 0 degree
+  fillIn(dark_yellow);
+  triangle(30, 50, 60, 80, 30, 80); //tail shadow
+  ellipse(60, 50, dbod, dbod); //bod shadow
 
-  rotate(-45);
-  fillIn(yell);
-  ellipse(70, 40, dhead, dhead);
+  fillIn(yellow);
+  ellipse(61, 49, dbod-3, dbod-3); //bod light
+  triangle(32.5, 53, 60, 77.5, 33, 77); //tail light
+
+  fillIn(dark_orange);
+  triangle(70, 20, 90, 40, 90, 20); //beak shadow
+
+  fillIn(orange);
+  triangle(70, 20, 90, 40, 89, 21); //beak light
+
+  fillIn(dark_yellow);
+  ellipse(70, 40, dhead, dhead); //head shadow
+
+  fillIn(yellow);
+  ellipse(71, 39, dhead-2, dhead-2); //head light
+
+  rotate(-15); //-15 degree
+  fillIn(black);
+  ellipse(57, 40, eyew, eyeh); //eye l
+
+  rotate(30); //15 degree
+  ellipse(95, 20, eyeh, eyew); //eye r
+
+  rotate(-15); //reset angle - 0 degree
+}
+
+function lily() {
 
 }
 
-
 function my_symbol() {
+  rotate(mover);
+  translate(movex, movey, 0);
+  scale(moves, moves, moves);
   ducky();
 }
