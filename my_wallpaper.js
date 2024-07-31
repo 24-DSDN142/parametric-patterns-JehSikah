9d791b581f367517ea9c2b503256b57e45495406
JehSikah <jess.tia.merriman@gmail.com>
2024-07-31 22:56:10 +1200

    total revamp n more functions

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,24 +1,40 @@
 //your parameter variables go here!
+let palette = 1;
 
+let rot_ducky = 0; //rotate ducky (0-360)
+let x_ducky = 60; //move ducky on x axis
+let y_ducky = 60; //move ducky on y axis
+let scal_ducky = 1; //scale ducky
+
+let rot_lily = 0; //rotate lilypad (0-360)
+let x_lily = 160; //move lilypad on x axis
+let y_lily = 160; //move lilypad on y axis
+let scal_lily = 1; //scale lilypad
+
+
+let dbod = 60; //ducky body size
+let lilysize = 80; //lilypad size
+let lilysplit = 100; //lilypad split location (0-360)
 
 
 //colour variables
 let green;
 let dark_green;
-let red;
-let white;
 let black;
 let yellow;
 let dark_yellow;
 let orange;
 let dark_orange;
+let blue;
 let dark_blue;
+let white;
+let grey;
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
   //DEVELOP_GLYPH, GRID_WALLPAPER, GLIDE_WALLPAPER
   pWallpaper.resolution(FIT_TO_SCREEN);
-  //FIT_TO_SCREEN, NINE_LANDSCAPE, NINE_PORTRAIT, A4, A3, 
+  //FIT_TO_SCREEN, NINE_LANDSCAPE, NINE_PORTRAIT, A4, A3 (for favourite wallpaper)
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
@@ -27,6 +43,7 @@ function setup_wallpaper(pWallpaper) {
   pWallpaper.grid_settings.row_offset  = 0;
 
   //Defining colours
+  //Palette 1
   green = color(110, 230, 130);
   dark_green = color(100, 200, 130);
   black = color(80);
@@ -34,11 +51,27 @@ function setup_wallpaper(pWallpaper) {
   dark_yellow = color(230, 210, 100);
   orange = color(255, 180, 100);
   dark_orange = color(235, 160, 85);
+  blue = color(46, 227, 255);
   dark_blue = color(32, 165, 186);
+  white = color(222, 250, 255);
+  grey = color(190, 220, 222); 
+  
+  //Changing colour palette
+  if (palette == 1) {
+    dbod_main = yellow;
+    dbod_shad = dark_yellow;
+    beak_main = orange;
+    beak_shad = dark_orange;
+    water = blue;
+    water_shad = dark_blue;
+    lily_main = green;
+    lily_shad = dark_green;
+  }
+
 }
 
 function wallpaper_background() {
-  background(46, 227, 255); //light blue
+  background(water); //water colour
 }
 
 function fillIn(colour) {
@@ -46,124 +79,102 @@ function fillIn(colour) {
   stroke(colour);
 }
 
-let dhead = 40;
-let dbod = 60;
-let eyew = 8;
-let eyeh = 3;
 function ducky() {
-  rotate(45); //wings on angle - 45 degree
-  fillIn(dark_yellow);  
-  ellipse(55.5, 0, dbod/3, 50); //left wing shadow
-  ellipse(100, 0, dbod/3, 50); //right wing shadow
+  let dhead = 40;
+  let eyew = 8;
+  let eyeh = 3;
 
-  fillIn(yellow);
-  ellipse(55.5, 0, dbod/3.5, 45); //left wing light
-  ellipse(100, 0, dbod/3.5, 45); //right wing light
+  fillIn(dbod_shad);
+  ellipse(-22.5, 5, dbod / 3, 50); //left wing shadow
+  ellipse(22.5, 5, dbod / 3, 50); //right wing shadow
 
-  rotate(-45); //reset angle - 0 degree
-  fillIn(dark_yellow);
-  triangle(30, 50, 60, 80, 30, 80); //tail shadow
-  ellipse(60, 50, dbod, dbod); //bod shadow
+  fillIn(dbod_main);
+  ellipse(-22.5, 4, dbod / 3.5, 45); //left wing 
+  ellipse(22.5, 4, dbod / 3.5, 45); //right wing
 
-  fillIn(yellow);
-  ellipse(61, 49, dbod-3, dbod-3); //bod light
-  triangle(32.5, 53, 60, 77.5, 33, 77); //tail light
+  fillIn(dbod_shad);
+  triangle(-25, 17, 25, 17, 0, 40); //tail shadow
+  ellipse(0, 0, dbod, dbod); //body shadow
 
-  fillIn(dark_orange);
-  triangle(70, 20, 90, 40, 90, 20); //beak shadow
+  fillIn(dbod_main);
+  triangle(-22, 17.5, 22, 17.5, 0, 36.5); //tail
+  ellipse(0, -1, dbod - 3, dbod - 3); //body 
 
-  fillIn(orange);
-  triangle(70, 20, 90, 40, 89, 21); //beak light
+  fillIn(beak_shad);
+  triangle(-16, -27, 16, -27, 0, -43); //beak shadow
 
-  fillIn(dark_yellow);
-  ellipse(70, 40, dhead, dhead); //head shadow
+  fillIn(beak_main);
+  triangle(-15, -27, 15, -27, 0, -41); //beak
 
-  fillIn(yellow);
-  ellipse(71, 39, dhead-2, dhead-2); //head light
+  fillIn(dbod_shad);
+  ellipse(0, -15, dhead, dhead); //head shadow
 
-  rotate(-15); //-15 degree
+  fillIn(dbod_main);
+  ellipse(0, -16, dhead - 2, dhead - 2); //head
+
   fillIn(black);
-  ellipse(57, 40, eyew, eyeh); //eye l
-
-  rotate(30); //15 degree
-  ellipse(95, 20, eyeh, eyew); //eye r
-
-  rotate(-15); //reset angle - 0 degree
-}
-
-let lilylocx = 150; //lilypad x coord
-let lilylocy = 130; //lilypad y coord
-let lilysize = 80; //param //lilypad size
-let lilysplit = 120; //param //lilypad split location (degrees)
-function lily() {
-  fillIn(dark_blue);
-  arc(lilylocx, lilylocy+5, lilysize, lilysize, lilysplit, lilysplit-25); //water shadow
-
-  fillIn(dark_green);
-  arc(lilylocx, lilylocy, lilysize, lilysize, lilysplit, lilysplit-25); //lilypad shadow
-
-  fillIn(green);
-  arc(lilylocx, lilylocy, lilysize-7, lilysize-7, lilysplit+7, lilysplit-32); //lilypad light
-}
-
-let duckynum = 1; //number of ducks
-let duckyrot = 70; //rotate duck (0 - 360 degrees)
-let duckyx = 100; //move duck on x
-let duckyy = 0; //move duck on y
-let duckys = 1; //scale duck - default 1
-
-function duckyshad() { 
-  rotate(45);
-  fillIn(dark_blue);
-  ellipse(82, 0, dbod+dbod/10, dbod+dbod/3); //water shadow
-  rotate(-45);
   push();
-  rotate(duckyrot);
-  translate(duckyx, duckyy, 0);
+  rotate(45);
+  ellipse(-28, -10, eyeh, eyew);
+  rotate(-90);
+  ellipse(28, -10, eyeh, eyew);
   pop();
 }
 
-let lilynum = 5; //number of lilypads
-let lilyrot = 120; //rotate lilypad
-let lilyx = 100; //move lilypad on x
-let lilyy = 80; //move lilypad on y
-let lilys = 0.7; //scale lilypad - default 1
+function shad(x, y, thing, scale) {
 
-function lilyshad() {
+  let shad_dist = 7.5 * scale; //distance of shadows
+  let shad_ang = 70; //angle of shadows
 
+  //ew math
+  var x_shad = x + shad_dist * cos(shad_ang);
+  var y_shad = y + shad_dist * sin(shad_ang);
+
+  push(); 
+  fillIn(water_shad);
+
+  if (thing == "duck") { //duck shadow if duck
+    ellipse(x_shad, y_shad, dbod + dbod / 10, dbod + dbod / 3);
+  } else if (thing == "lily") { //lilypad shadow if lilypad
+    arc(x_shad, y_shad, lilysize, lilysize, lilysplit, lilysplit - 25);
+  }
+
+  pop();
 }
 
-var orbitCenterX = 100;
-var orbitCenterY = 100;
-var orbitRadius = 50;
-var angle = 45;
+function lilyp() {
+  fillIn(lily_shad);
+  arc(0, 0, lilysize, lilysize, lilysplit, lilysplit - 25); //lilypad shadow
+
+  fillIn(lily_main);
+  arc(0, 0, lilysize - 7, lilysize - 7, lilysplit + 7, lilysplit - 32); //lilypad
+}
+
+let ducky_num = 3;
 
 function my_symbol() {
 
-  var x = orbitCenterX + orbitRadius * cos(angle);
-  var y = orbitCenterY + orbitRadius * sin(angle);
   
-  ellipse(orbitCenterX, orbitCenterY, 50, 50);
-  ellipse(x, y, 10, 10);
-
-  /* for (let i = 0; i < duckynum; i += 1) {
+  for (i = 0; i < ducky_num; i += 1) {
     push();
-    rotate(duckyrot);
-    translate(duckyx, duckyy, 0);
-    scale(duckys);
-    duckyshad();
+    translate(x_ducky, y_ducky, 0);
+    rotate(rot_ducky);
+    scale(scal_ducky);
+    shad(x_ducky, y_ducky, "duck", scal_ducky); //figure how to rotate on spot
     ducky();
     pop();
-  } */
+  }
   
+  shad(x_lily, y_lily, "lily", scal_lily); //figure how to rotate on spot
   
- /* for (let i = 0; i < lilynum; i += 1) {
-    push();
-    rotate(lilyrot);
-    translate(lilyx, lilyy, 0);
-    scale(lilys);
-    lily();
-    pop();
-  } */
+  push();
+  scale(scal_lily);
+  translate(x_lily, y_lily, 0);
+  rotate(rot_lily);
+  lilyp();
+  pop();
+  
+  rot_ducky += 45;
+  rot_lily += 45;
   
 }
